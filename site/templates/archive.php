ba4ac53f1ca61a40ae7c8cd9daf719605718d561
6ui11em <6ui11em1@gmail.com>
2015-06-16 22:31:45 +0200

    Archive template
    Signed-off-by: 6ui11em <6ui11em1@gmail.com>

--- site/templates/archive.php
+++ site/templates/archive.php
@@ -1,58 +1,62 @@
 <?php snippet( 'header') ?>
 
-<section id="blog">
+<div class="container">
+	<div class="row main">
+		<section id="archive" class="archive col-sm-9">
 
-<header>
-  <h1>Archive<? e(isset($archiveTitle), $archiveTitle) ?>:</h1>
-</header>
+			<header>
+				<h1>Archive<? e(isset($archiveTitle), $archiveTitle) ?>:</h1>
+			</header>
 
-<?php if($posts->count()): ?>
+			<?php if($posts->count()): ?>
 
-<?php
-$tmpDate = getdate(0);
-foreach($posts as $post): ?>
-  <?php if(!isset($year)): ?>
-    <?php
-      $date = getdate($post->date());
-      if ($tmpDate['year'] != $date['year']): ?>
-        <header class="archive-year">
-          <h2>
-            <?= $date['year'] ?>
-          </h2>
-        </header>
-    <?php endif ?>
-  <?php endif ?>
+			<?php
+			$tmpDate = getdate(0);
+			foreach($posts as $post): ?>
+				<?php if(!isset($year)): ?>
+					<?php
+						$date = getdate($post->date());
+						if ($tmpDate['year'] != $date['year']): ?>
+							<header class="archive-year">
+								<h2>
+									<?= $date['year'] ?>
+								</h2>
+							</header>
+					<?php endif ?>
+				<?php endif ?>
 
-  <article>
-    <header>
-      <h1>
-        <a href="<?= getPostUrl($post) ?>">
-          <?= $post->title()->html() ?>
-        </a>
-      </h1>
-    </header>
+				<article>
+					<header>
+						<h1>
+							<a href="<?= getPostUrl($post) ?>">
+								<?= $post->title()->html() ?>
+							</a>
+						</h1>
+					</header>
 
-    <?php snippet('post-footer', array('post' => $post)) ?>
+					<?php snippet('post-footer', array('post' => $post)) ?>
 
-  </article>
-<?php
-if(!isset($year)) $tmpDate = $date;
-endforeach ?>
+				</article>
+			<?php
+			if(!isset($year)) $tmpDate = $date;
+			endforeach ?>
 
 
-<?php else: ?>
+			<?php else: ?>
 
-  <?= $page->noposts()->kirbytext() ?>
+				<?= $page->noposts()->kirbytext() ?>
 
-<?php endif ?>
+			<?php endif ?>
 
-<?php snippet('nav-pagination') ?>
+			<?php snippet('nav-pagination') ?>
 
-</section>
-
-<?php snippet('archives', array('dates'      => true,
-                                'authors'    => true,
-                                'tags'       => true,
-                                'categories' => true)) ?>
-
+		</section>
+		<aside id="sidebar" class="sidebar col-sm-3">
+			<?php snippet('archives', array('dates'      => true,
+																			'authors'    => true,
+																			'tags'       => true,
+																			'categories' => true)) ?>
+		</aside>
+	</div>
+</div>		
 <?php snippet( 'footer') ?>
