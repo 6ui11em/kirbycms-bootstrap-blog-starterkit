b359ac5592fd70e1c278cd2106ecae5a820fe3b4
6ui11em <6ui11em1@gmail.com>
2015-06-16 12:31:25 +0200

    Add blog snippets
    Signed-off-by: 6ui11em <6ui11em1@gmail.com>

---
+++ site/snippets/archives.php
@@ -0,0 +1,22 @@
+<?php
+if(!isset($dates))      $dates      = false;
+if(!isset($authors))    $authors    = false;
+if(!isset($tags))       $tags       = false;
+if(!isset($categories)) $categories = false;
+if(!isset($class))      $class    = false;
+?>
+
+<section id="archives" <?php if($class) echo 'class="' . $class . '"' ?>>
+  <header>
+    <h1>Archive options:</h1>
+  </header>
+
+  <?php if($tags) snippet('archive-tags') ?>
+
+  <?php if($categories) snippet('archive-categories') ?>
+
+  <?php if($dates) snippet('archive-dates') ?>
+
+  <?php if($authors) snippet('archive-authors') ?>
+
+</section>
